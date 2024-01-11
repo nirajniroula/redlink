fcbc439ba595b3ea9e29be366aa3c0f485f4ebd7
Niraj Niroula <niniroula@gmail.com>
2024-01-11 22:09:15 +0545

    add new fields to edit profile screen

--- src/screens/app/EditProfileScreen.tsx
+++ src/screens/app/EditProfileScreen.tsx
@@ -5,14 +5,62 @@ import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
 import { Colors } from '../../utils/colors';
 import { Button, TextInput } from 'react-native-paper';
 import { UserContext } from '../../context/UserContext';
-import { Picker } from '@react-native-picker/picker';
 import { updateUser } from '../../firebase';
 import { AppUser } from '../../context/types';
+import DropDownPicker from 'react-native-dropdown-picker';
+
+const iso3166 = require('iso-3166-1-alpha-2');
+const countryEmoji = require('country-emoji');
+
+const allCountries = iso3166.getCodes().map((code: string) => {
+  const countryName = iso3166.getCountry(code);
+  const flag = countryEmoji.flag(code);
+  return { label: `${flag} ${countryName}`, value: code };
+});
+
+const bloodGroupsList = [
+  {
+    label: 'A+',
+    value: 'A+',
+  },
+  {
+    label: 'A-',
+    value: 'A-',
+  },
+  {
+    label: 'B+',
+    value: 'B+',
+  },
+  {
+    label: 'B-',
+    value: 'B-',
+  },
+  {
+    label: 'AB+',
+    value: 'AB+',
+  },
+  {
+    label: 'AB-',
+    value: 'AB-',
+  },
+  {
+    label: 'O+',
+    value: 'O+',
+  },
+  {
+    label: 'O-',
+    value: 'O-',
+  },
+];
+
 const EditProfileScreen = () => {
   const { state, dispatch } = React.useContext<any>(UserContext);
   const { user } = state;
   const defaultEditUser = user;
   const [editUser, setEditUser] = React.useState(defaultEditUser);
+  const [loading, setLoading] = React.useState(false);
+  const [showBloodDropDown, setShowBloodDropDown] = React.useState(false);
+  const [showCountryDropDown, setShowCountryDropDown] = React.useState(false);
 
   const areUsersDifferent = (obj1: AppUser, obj2: AppUser): boolean => {
     const keys1 = Object.keys(obj1) as (keyof AppUser)[];
@@ -25,8 +73,12 @@ const EditProfileScreen = () => {
 
   const onSubmitPress = async () => {
     console.log('...', user, editUser);
-    if (areUsersDifferent(user, editUser)) {
-      const res = await updateUser(editUser.userId, editUser);
+    if (editUser.userId && areUsersDifferent(user, editUser)) {
+      setLoading(true);
+
+      const res = await updateUser(editUser.userId, editUser).finally(() => {
+        setLoading(false);
+      });
       if (res) {
         dispatch({ type: 'SET_USER', payload: editUser });
       }
@@ -77,19 +129,51 @@ const EditProfileScreen = () => {
               setEditUser({ ...editUser, email: text.trim() })
             }
           />
-          <Picker
-            style={styles.textInput}
-            placeholder="Blood Group"
-            selectedValue={editUser.bloodGroup}
-            onValueChange={(itemValue, itemIndex) =>
-              setEditUser({ ...editUser, email: itemValue })
-            }
+
+          <View
+            style={{
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              display: 'flex',
+              flexDirection: 'row',
+              width: '100%',
+            }}
           >
-            <Picker.Item label="A+" value="A+" />
-            <Picker.Item label="B+" value="B+" />
-          </Picker>
+            <DropDownPicker
+              open={showBloodDropDown}
+              value={editUser.bloodGroup}
+              items={bloodGroupsList}
+              setOpen={setShowBloodDropDown}
+              setValue={() => {}}
+              onSelectItem={(item) => {
+                setEditUser({ ...editUser, bloodGroup: item.value });
+                console.log('...', item.value);
+              }}
+              placeholder={'Blood Group'}
+              containerStyle={{ width: '36%' }}
+            />
+
+            <DropDownPicker
+              open={showCountryDropDown}
+              value={editUser.country}
+              items={allCountries}
+              searchable={true}
+              setOpen={setShowCountryDropDown}
+              setValue={() => {}}
+              onSelectItem={(item) => {
+                setEditUser({ ...editUser, country: item.value });
+              }}
+              placeholder={'Country'}
+              containerStyle={{ width: '60%' }}
+            />
+          </View>
         </View>
-        <Button mode="contained" onPress={onSubmitPress}>
+        <Button
+          mode="contained"
+          onPress={onSubmitPress}
+          loading={loading}
+          disabled={loading}
+        >
           Submit
         </Button>
       </View>
